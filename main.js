1d0727dc3aa7bb7f07e0d23deeb2b623c16e5078
brittrademaker <rademaker.britt@hotmail.com>
2019-05-18 14:15:14 +0200

    360deg Rotate

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ addBlacks.forEach(function (addBlack) {
         addBlack.classList.toggle('fill-red');
         addBlack.classList.toggle('fill-black');
     });
-})
+});
 
 
 var addReds = document.querySelectorAll('.fill-black');
@@ -14,11 +14,11 @@ addReds.forEach(function (addRed) {
         addRed.classList.toggle('fill-red');
         addRed.classList.toggle('fill-black');
     });
-})
+});
 
-var addScale = document.querySelector('.grid-container');
+var addScale = document.querySelector('body');
 document.addEventListener('keypress', function (e) {
     if (e.keyCode === 32) {
-        addScale.classList.toggle('scale_up')
+        addScale.classList.toggle('scale_up');
     }
-})
+});
